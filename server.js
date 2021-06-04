0e84e68b23e2a870a441fac5eb1761f3712dbeae
lukegammon <>
2021-06-04 20:55:57 +0000

    server.js final

--- server.js
+++ server.js
@@ -9,23 +9,23 @@ app.get("/", (req,res) => {
 })
 
 app.get("/api/:date?", (req, res) => {
+  let finalObj = {}
   if(req.params.date.includes("-")) {
     const input = req.params.date;
-    console.log(input);
-    res.json({
-      unix: new Date(input).getTime(),
-      utc: new Date(input).toUTCString()
-    });
+    finalObj.unix = new Date(input).getTime(),
+    finalObj.utc = new Date(input).toUTCString()
   } else {
-    const input = new Date(parseInt(req.params.date)).getTime();
-    const utcDate = new Date(input).toUTCString();
-    console.log(utcDate); 
-    res.json( {
-      unix: input,
-      utc: utcDate
-    })
+      const input = new Date(parseInt(req.params.date)).getTime();
+      const utcDate = new Date(input).toUTCString();
+      finalObj.unix = input,
+      finalObj.utc = utcDate
+    }
+
+  if(!finalObj.unix) {
+    res.json({ error : "Invalid Date" });
   }
-})
+  res.json(finalObj);
+});
 
 
 app.listen(process.env.PORT || 5000, console.log("Started server"));
